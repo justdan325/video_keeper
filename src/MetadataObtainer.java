2a5dfa93dffb03ae92689c52ce872b99879cb4c9
dan <dan@dan-XPS-8950>
2025-10-05 16:11:40 -0400

    Unit test update.

--- src/MetadataObtainer.java
+++ src/MetadataObtainer.java
@@ -58,7 +58,7 @@ public class MetadataObtainer {
 	
 	public static void main(String[] args) {
 //		System.out.println(fetchHtml("https://odysee.com/win11:6d73df3083e0f634b18f54521763184b47980d8a"));
-		final String URL = "https://rumble.com/v6ziyjy-they-met-on-x-with-goodtweetman-and-catholic-engineer-crash-canon-podcast-e.html?e9s=src_v1_cbl%2Csrc_v1_ucp_a";
+		final String URL = "https://www.twitch.tv/videos/2583383258";
 		MetadataObtainer o = new MetadataObtainer(URL);
 		System.out.println("URL provided: [" + URL + "]");
 		System.out.println("Is supported: [" + isSupported(URL, true) + "]");
