f0095821e5399352432251a042ddd90c9e17db4f
dan <dan@dan-XPS-8950>
2025-11-14 23:00:38 -0500

    Yet more unit test updates!

--- src/Test.java
+++ src/Test.java
@@ -50,11 +50,11 @@ public class Test {
 	private static final String CHNL_YT_PREVIEW 	= "The B1M on YouTube";
 	private static final String TIME_YT_PREVIEW 	= "06:34";
 	
-	private static final String URL_YT_CHANNEL		= "https://www.youtube.com/@DaveMcRaeOfficial/videos";
+	private static final String URL_YT_CHANNEL		= "https://www.youtube.com/@daviddifranco";
 	private static final String TYPE_YT_CHANNEL 	= "YouTube Channels using \"@\"";
-	private static final String TTL_YT_CHANNEL 		= "DaveMcRaeOfficial";
-	private static final String DATE_YT_CHANNEL 	= "30K Subscribers ~ 1.5K Videos";
-	private static final String CHNL_YT_CHANNEL 	= "DaveMcRaeOfficial on YouTube";
+	private static final String TTL_YT_CHANNEL 		= "daviddifranco";
+	private static final String DATE_YT_CHANNEL 	= "215K Subscribers ~ 3.3K Videos";
+	private static final String CHNL_YT_CHANNEL 	= "daviddifranco on YouTube";
 	private static final String TIME_YT_CHANNEL 	= "";
 	
 	private static final String URL_YT_CHANNEL_2	= "https://www.youtube.com/c/GreatDepressionCooking";
