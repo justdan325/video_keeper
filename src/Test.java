4c92ef68b1d9794e0e9025138473e52ee8de5d3c
dan <dan@dan-Inspiron-5567>
2026-02-20 16:52:42 -0500

    Unit test update.

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@
  */
 public class Test {
 	private static final boolean EXIT_UPON_FAILURE = false; //if true, will exit testing when a link fails. Setting false can be handy when testing wonky sites.
-	private static final boolean USE_YTDLP = true;
+	private static final boolean USE_YTDLP = false;
 	
 	//YouTube
 	private static final int	YT_GROUP_NO			= 1;
@@ -251,10 +251,12 @@ public class Test {
 	
 	private boolean testLink(String url, String linkType, String title, String date, String channel, String time) {
 		DataModel model = new DataModel();
-		MetadataObtainer obtainer = new MetadataObtainer(model, url);
+		MetadataObtainer obtainer;
 		boolean success = false;
 		
 		model.setUseYtdlp(USE_YTDLP);
+		
+		obtainer = new MetadataObtainer(model, url);
 
 		if (obtainer.isSupported()) {
 			if (obtainer.isUrlError()) {
